0286ba86c16120763d96865a5026a50fa9492fdb
Mike AbiEzzi <mjezzi@gmail.com>
2013-09-15 20:15:18 -0600

    Add verbosity to populate script

--- .populate.sh
+++ .populate.sh
@@ -1,26 +1,34 @@
 function populate { 
   local BACKUPS_DIR="".dotfile_backups/"`date +"%T"`"
+  echo "BACKUPS_DIR=$BACKUPS_DIR"
   local BACKUPS_PATH="$HOME/$BACKUPS_DIR"
+  echo "BACKUPS_PATH=$BACKUPS_PATH"
+  echo ""
 
   for f in ~/.dotfiles/*
   do
     local DEST_FILE=".${f##*/}"
+    echo "DEST_FILE=$DEST_FILE"
     local DEST_PATH="$HOME/$DEST_FILE"
+    echo "DEST_PATH=$DEST_PATH"
 
-    local SRC_FILE="$f"
+    local SRC_PATH="$f"
+    echo "SRC_PATH=$SRC_PATH"
 
     if [ -f $DEST_PATH ] || [ -d $DEST_PATH ]; then
       if [ ! -d $BACKUPS_PATH ]; then
+        echo "mkdir $BACKUPS_PATH"
         mkdir $BACKUPS_PATH
-        echo "Created ~/$BACKUPS_DIR directory\n"
       fi
 
-      mv "$DEST_PATH" "$BACKUPS_PATH/$DEST_FILE"
-      echo "Moved ~/$DEST_FILE to ~/$BACKUPS_DIR"
+      echo "mv "$DEST_PATH" "$BACKUPS_PATH/$DEST_FILE""
+      mv "$DEST_PATH" "$BACKUPS_PATH$DEST_FILE"
     fi
 
-    ln -s $SRC_FILE $DEST_PATH
-    echo "Created ~/$DEST_FILE\n"
+    echo "ln -s $SRC_PATH $DEST_PATH"
+    ln -s $SRC_PATH $DEST_PATH
+
+    echo ""
   done
 }
 
